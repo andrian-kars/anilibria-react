22b222a1a12af218fe47c386583f5b35e6c323f7
andrian-kars <andrian.karsanashvili@gmail.com>
2021-11-27 14:26:21 +0200

    :hammer: first layout

--- src/pages/Catalog/Catalog.tsx
+++ src/pages/Catalog/Catalog.tsx
@@ -1,8 +1,10 @@
 import { useEffect } from 'react'
 import { AnimeItem } from '../../components/common'
+import { Field, Formik } from 'formik'
 import { useAppDispatch, useAppSelector } from '../../hooks/redux'
 import { fetchNewAnimeListForCatalog } from '../../store/reducers/CatalogSlice'
 import s from './Catalog.module.scss'
+import { NavLink } from 'react-router-dom'
 
 export const Catalog: React.FC = () => {
   const { animeListForCatalog, isLoading, error } = useAppSelector((state) => state.catalogReducer)
@@ -15,10 +17,63 @@ export const Catalog: React.FC = () => {
     !animeListForCatalog && dispatch(fetchNewAnimeListForCatalog())
   }, [dispatch, animeListForCatalog])
 
+  const searchSubmit = (data: any) => {
+    console.log(data)
+  }
   // TODO: loading, error
   return (
     <section className={s.content}>
-      <div className={s.search}></div>
+      <div className={s.search}>
+        <Formik
+          initialValues={{
+            genre: null,
+            year: null,
+            season: null,
+            type: 'new',
+            releaseFinished: false,
+          }}
+          onSubmit={searchSubmit}
+        >
+          {/* {({ values, handleChange, handleBlur, handleSubmit, isSubmitting }) => ( */}
+          {({ handleSubmit, isSubmitting }) => (
+            <form onSubmit={handleSubmit}>
+              <div className={s.selects}>
+                <Field as="select" name="genres">
+                  <option value="null">All Users</option>
+                  <option value="true">Followed</option>
+                  <option value="false">Unfollowed</option>
+                </Field>
+                <Field as="select" name="years">
+                  <option value="null">All Users</option>
+                  <option value="true">Followed</option>
+                  <option value="false">Unfollowed</option>
+                </Field>
+                <Field as="select" name="seasons">
+                  <option value="null">All Users</option>
+                  <option value="true">Followed</option>
+                  <option value="false">Unfollowed</option>
+                </Field>
+              </div>
+              <div className={s.searchBottom}>
+                <div className={s.submits}>
+                  <button type="submit" disabled={isSubmitting}>
+                    New
+                  </button>
+                  <button type="submit" disabled={isSubmitting}>
+                    Show
+                  </button>
+                  <button type="submit" disabled={isSubmitting}>
+                    Release over
+                  </button>
+                </div>
+                <NavLink className={s.alphabet} to={'/pages/alphabet'}>
+                  АЛФАВИТНЫЙ УКАЗАТЕЛЬ
+                </NavLink>
+              </div>
+            </form>
+          )}
+        </Formik>
+      </div>
       <div className={s.animes}>
         {isLoading && <h1>Loading...</h1>}
         {error && <h1>{error}</h1>}
