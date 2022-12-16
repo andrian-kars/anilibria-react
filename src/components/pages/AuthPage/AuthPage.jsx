adc0e2857d51d5fea2fb165e0fd006eeb216dca5
andrian-kars <andrian.karsanashvili@gmail.com>
2022-12-16 20:54:13 +0200

    :sparkles: AuthPageForm

--- src/components/pages/AuthPage/AuthPage.jsx
+++ src/components/pages/AuthPage/AuthPage.jsx
@@ -1,74 +1,47 @@
 // import cn from 'classnames';
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import s from './AuthPage.module.scss';
-import { Text, Heading, Button } from 'src/components/common';
-import { useEffectOnce } from 'src/hooks';
+import { Text, Heading } from 'src/components/common';
 import store from 'src/store/authStore';
 import { observer } from 'mobx-react-lite';
-import { useNavigate } from 'react-router-dom';
-
-const PAGE_TITLE = 'Логін';
+import { useNavigate, useParams } from 'react-router-dom';
+import { AuthPageForm } from './AuthPageForm';
+import { INITIAL_PAGE_PATH } from 'src/constants';
+import { useCallback } from 'react';
 
 export const AuthPage = observer(() => {
   const navigate = useNavigate();
+  const authType = useParams().authType;
+  const { isAuth, user } = store;
+  const isLogin = authType === 'login';
+  const pageTitle = isLogin ? 'login' : 'registration';
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  function handleEmailChagne(event) {
-    setEmail(event.target.value);
-  }
-
-  function handlePasswordChagne(event) {
-    setPassword(event.target.value);
-  }
-
-  function handleLogin() {
+  const handleLogin = useCallback((email, password) => {
     store.login(email, password);
-  }
+  });
 
-  function handleLogout() {
-    store.logout();
-  }
-
-  useEffectOnce(() => {
-    if (store.isAuth) {
-      navigate('/');
-    }
-
-    document.title = PAGE_TITLE;
+  const handleRegistration = useCallback((email, password) => {
+    store.registration(email, password);
   });
 
   useEffect(() => {
-    if (store.isAuth) {
-      navigate('/');
+    if (isAuth || (authType !== 'login' && authType !== 'registration')) {
+      navigate(INITIAL_PAGE_PATH);
+    } else {
+      document.title = pageTitle;
     }
-  }, [store.isAuth]);
+  }, [isAuth, authType]);
 
   return (
     <section className={s.content}>
       <Heading type="h3" content="Login" className={s.heading} />
       <Text className={s.text}>
-        {store.isAuth ? `user is logged in as ${store.user.email}` : 'user is not logged in'}
+        {isAuth ? `user is logged in as ${user.email}` : 'user is not logged in'}
       </Text>
-      <div>
-        <input
-          onChange={handleEmailChagne}
-          type="text"
-          placeholder="Enter your email"
-          value={email}
-        />
-        <input
-          onChange={handlePasswordChagne}
-          type="password"
-          placeholder="Enter your password"
-          value={password}
-        />
-        <Button onClick={handleLogin}>Login</Button>
-        <Button onClick={handleLogout}>Logout</Button>
-        <Button>Registration</Button>
-      </div>
-      <Button>get users</Button>
+      <AuthPageForm
+        buttonText={pageTitle}
+        buttonClick={isLogin ? handleLogin : handleRegistration}
+      />
     </section>
   );
 });
