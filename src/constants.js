9e4f1671a92b2c8d195d4a229d007041d22a1e28
andrian-kars <andrian.karsanashvili@gmail.com>
2022-12-17 15:27:16 +0200

    :sparkles: connect hosted API

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 // general
-export const NODE_API_URL = 'http://localhost:5069/api/';
+export const NODE_API_URL = 'http://44.211.77.55:8080/api/';
 // paths
 export const INITIAL_PAGE_PATH = '/';
 export const AUTH_PAGE_PATH = '/auth/:authType';
