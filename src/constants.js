ef880977759b8332223c33c775a2756c8bbfbf99
andrian-kars <andrian.karsanashvili@gmail.com>
2023-01-18 16:09:50 +0200

    :sparkles: new node-api url

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 // general
-export const NODE_API_URL = 'http://44.211.77.55:8080/api/';
+export const NODE_API_URL = 'https://stage.andrian-kars.space/api/';
 export const ANILIBRIA_API_URL = 'https://api.anilibria.tv/v3/';
 export const ANILIBRIA_STORAGE_URL = 'https://anilibria.tv';
 // paths
