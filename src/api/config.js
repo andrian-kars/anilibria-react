f83a169f67f3ed7826322e570998d67a865514a6
andrian-kars <andrian.karsanashvili@gmail.com>
2023-01-18 19:55:28 +0200

    :sparkles: remove old anilibria api instance

--- src/api/config.js
+++ src/api/config.js
@@ -2,27 +2,6 @@ import axios from 'axios';
 import { ANILIBRIA_API_URL, NODE_API_URL, STORAGE_TOKEN, ERROR_CODE_401 } from 'src/constants';
 import { checkAuthHelper } from './node/authService';
 
-export const anilibriaApiInstance1 = (path, params = null, responseModifyCallback = null) => {
-  let paramsToSend = '';
-
-  if (params) {
-    const configurePrams = (params) => {
-      // Sample implementation of query string building
-      let result = '';
-      Object.keys(params).forEach((key) => {
-        result += `${key}=${encodeURIComponent(params[key])}&`;
-      });
-      return result.substr(0, result.length - 1);
-    };
-
-    paramsToSend = configurePrams(params);
-  }
-
-  return fetch(`https://api.anilibria.tv/v2/${path}?` + paramsToSend)
-    .then((res) => res.json())
-    .then((data) => (responseModifyCallback ? responseModifyCallback(data) : data));
-};
-
 export const anilibriaApiInstance = axios.create({
   baseURL: ANILIBRIA_API_URL,
 });
