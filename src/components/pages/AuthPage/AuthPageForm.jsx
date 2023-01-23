a40d14ecde9f668ef7ccc46bb6a059efe46206f1
Viktor <yadelskyiviktor@gmail.com>
2023-01-23 19:07:39 +0200

    :sparkles:remove husky moving auth SVG to component add show password icon

--- src/components/pages/AuthPage/AuthPageForm.jsx
+++ src/components/pages/AuthPage/AuthPageForm.jsx
@@ -6,6 +6,7 @@ import { useIntl } from 'react-intl';
 import { TERMS_PAGE_PATH } from 'src/constants';
 import { emailSchema, passwordSchema } from './AuthPageSchema';
 import s from './AuthPage.module.scss';
+import { EmailIcon, ShowHidePasswordIcon } from './AuthSVGs';
 
 export const AuthPageForm = memo(({ buttonText, onSubmit, isLogin }) => {
   const { formatMessage } = useIntl();
@@ -44,10 +45,6 @@ export const AuthPageForm = memo(({ buttonText, onSubmit, isLogin }) => {
     setChecked(!checked);
   };
 
-  const showHidePasswordHandler = () => {
-    setPasswordShown(!passwordShown);
-  };
-
   return (
     <form className={s.formContent}>
       <div className={s.warningContainer}>
@@ -59,12 +56,7 @@ export const AuthPageForm = memo(({ buttonText, onSubmit, isLogin }) => {
         type="email"
         placeholder={formatMessage({ id: 'loginForm.placeholderEmail' })}
         value={email}
-        icon={
-          <svg viewBox="0 0 32 32">
-            <path d="M28,13a1,1,0,0,0-1,1v8a1,1,0,0,1-1,1H6a1,1,0,0,1-1-1V14a1,1,0,0,0-2,0v8a3,3,0,0,0,.88,2.12A3,3,0,0,0,6,25H26a3,3,0,0,0,2.12-.88A3,3,0,0,0,29,22V14A1,1,0,0,0,28,13Z" />
-            <path d="M15.4,18.8a1,1,0,0,0,1.2,0L28.41,9.94a1,1,0,0,0,.3-1.23,3.06,3.06,0,0,0-.59-.83A3,3,0,0,0,26,7H6a3,3,0,0,0-2.12.88,3.06,3.06,0,0,0-.59.83,1,1,0,0,0,.3,1.23ZM6,9H26a.9.9,0,0,1,.28,0L16,16.75,5.72,9A.9.9,0,0,1,6,9Z" />
-          </svg>
-        }
+        icon={<EmailIcon />}
       />
 
       <Input
@@ -73,22 +65,7 @@ export const AuthPageForm = memo(({ buttonText, onSubmit, isLogin }) => {
         placeholder={formatMessage({ id: 'loginForm.placeholderPassword' })}
         value={password}
         icon={
-          <svg
-            viewBox="0 0 64 64"
-            enableBackground="new 0 0 64 64"
-            onClick={showHidePasswordHandler}
-          >
-            <path
-              d="M32,15C11.169,15,0.769,30.242,0.336,30.891c-0.448,0.672-0.448,1.547,0,2.219C0.769,33.758,11.169,49,32,49
-		s31.231-15.242,31.664-15.891c0.448-0.672,0.448-1.547,0-2.219C63.231,30.242,52.831,15,32,15z M32,45
-		C16.493,45,7.234,35.322,4.512,31.996C7.225,28.663,16.436,19,32,19c15.507,0,24.766,9.678,27.488,13.004
-		C56.775,35.337,47.564,45,32,45z"
-            />
-            <path
-              d="M32,23c-4.963,0-9,4.038-9,9s4.037,9,9,9s9-4.038,9-9S36.963,23,32,23z M32,37c-2.757,0-5-2.243-5-5s2.243-5,5-5
-		s5,2.243,5,5S34.757,37,32,37z"
-            />
-          </svg>
+          <ShowHidePasswordIcon setPasswordShown={setPasswordShown} passwordShown={passwordShown} />
         }
       />
 
