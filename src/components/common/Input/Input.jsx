eb8613fdc38497024564ced589eb15bb1ca853ac
Viktor <yadelskyiviktor@gmail.com>
2023-01-25 17:21:25 +0200

    :wrench:delete caps lock functionality

--- src/components/common/Input/Input.jsx
+++ src/components/common/Input/Input.jsx
@@ -1,19 +1,9 @@
 import PropTypes from 'prop-types';
 import s from './Input.module.scss';
-import { memo, useState } from 'react';
+import { memo } from 'react';
 
 export const Input = memo(
   ({ type = 'text', value, placeholder, onChange, disabled = false, icon }) => {
-    const [isCapsLockOn, setIsCapsLockOn] = useState(false);
-
-    const checkCapsLock = (e) => {
-      if (e.getModifierState('CapsLock')) {
-        setIsCapsLockOn(true);
-      } else {
-        setIsCapsLockOn(false);
-      }
-    };
-
     return (
       <div className={s.wrapper}>
         <div className={s.icon}>{icon}</div>
@@ -23,16 +13,8 @@ export const Input = memo(
           value={value}
           placeholder={placeholder}
           onChange={onChange}
-          onKeyUp={checkCapsLock}
           disabled={disabled}
         />
-        <div className={s.caps}>
-          {isCapsLockOn && (
-            <svg viewBox="0 0 24 24">
-              <path d="M7.27 1.047a1 1 0 0 1 1.46 0l6.345 6.77c.6.638.146 1.683-.73 1.683H11.5v1a1 1 0 0 1-1 1h-5a1 1 0 0 1-1-1v-1H1.654C.78 9.5.326 8.455.924 7.816L7.27 1.047zM4.5 13.5a1 1 0 0 1 1-1h5a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1h-5a1 1 0 0 1-1-1v-1z" />
-            </svg>
-          )}
-        </div>
       </div>
     );
   },
